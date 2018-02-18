42b178b91b0e15509ee9f923ff4e9ae000ea7c8f
Donovon <donovonjenson@hotmail.com>
2018-02-18 13:24:14 -0800

    Initial React Native Setup

---
+++ login.js
@@ -0,0 +1,21 @@
+'use Strict';
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Login extends Component<Props> {
+
+	render() {
+		return (
+			<Text> Hello </Text>
+		);
+	}
+
+}
+
+module.exports = Login;
